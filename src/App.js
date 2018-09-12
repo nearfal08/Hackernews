f6b7f02a3a27c92fdc4ba8ce175fc75ef1f62304
wes <wes@onehat.com>
2018-09-11 19:37:39 -0500

    Updated through page 54

--- src/App.js
+++ src/App.js
@@ -21,26 +21,66 @@ const list = [
     },
   ];
   
+const userService = {
+  getUserName(user) {
+  return user.firstname + ' ' + user.lastname;
+  },
+};
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }  
+  
   render() { 
     return (
-      <div className="App">
-        {list.map(function(item) {
-          return (
+        <div className="App">
+          <form>
+            <input
+              type="text"
+              onChange={this.onSearchChange}
+            />
+          </form>
+          {this.state.list.map(item =>
             <div key={item.objectID}>
-            <span>
-            <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
-          ); 
-        })}
-      </div>
+          )}
+        </div>
     );
   }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+    
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+    
 }
 
 export default App;

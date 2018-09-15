90fadbabdddc43201bafa3b1626f699d0271a836
Weston Walker <walk8919@gmail.com>
2018-09-15 13:18:44 -0500

    page 82

--- src/App.js
+++ src/App.js
@@ -30,6 +30,44 @@ const userService = {
 function isSearched(searchTerm) {
   return item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 }
+
+// Functional stateless components
+const Button = ({ onClick, className = '', children }) => 
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
+
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form> 
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item => 
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+          <Button onClick={() => onDismiss(item.objectID)} className="button-inline">
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
    
 class App extends Component {
 
@@ -47,36 +85,23 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-        <div className="App">
-          <form>
-            <input
-              type="text"
+        <div className="page"> 
+          <div className="interactions">
+            <Search
               value={searchTerm}
-
               onChange={this.onSearchChange}
             />
-          </form> 
-          {list.filter(isSearched(searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          )}
+          </div> 
+          <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
         </div>
     );
   }
+
+  
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
@@ -91,3 +116,66 @@ class App extends Component {
 }
 
 export default App;
+
+
+// Class component version of the Search component
+
+// class Button extends Component {
+//   render() {
+//     const {
+//       onClick,
+//       className = '',
+//       children,
+//     } = this.props;
+
+//     return (
+//       <button
+//         onClick={onClick}
+//         className={className}
+//         type="button"
+//       >
+//       {children}
+//       </button>
+//     );
+//   }
+// }
+ 
+// class Search extends Component {
+//   render() {
+//     const { value, onChange } = this.props;
+//     return (
+//       <form>
+//         <input
+//           type="text"
+//           value={value}
+//           onChange={onChange}
+//         />
+//       </form>
+//     );
+//   }
+// }
+
+// class Table extends Component {
+//   render() {
+//     const { list, pattern, onDismiss } = this.props;
+//     return (
+//       <div>
+//         {list.filter(isSearched(pattern)).map(item => 
+//           <div key={item.objectID}>
+//             <span>
+//               <a href={item.url}>{item.title}</a>
+//             </span>
+//             <span>{item.author}</span>
+//             <span>{item.num_comments}</span>
+//             <span>{item.points}</span>
+//             <span>
+//             <Button onClick={() => onDismiss(item.objectID)}>
+//               Dismiss
+//             </Button>
+//             </span>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }

6bcd57811e4326f4304f26531f28b8eec056ed2e
Weston Walker <walk8919@gmail.com>
2018-10-04 18:47:22 -0500

    page110

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const Search = ({ value, onChange, onSubmit, children }) =>
     </button>
   </form> 
 
-const Table = ({ list, onDismiss }) =>
+const Table = ({ list, onDismiss }) => 
   <div className="table">
     {list.map(item => 
       <div key={item.objectID} className="table-row">
@@ -67,25 +67,33 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
     };
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-  }  
-  
-  setSearchTopStories(result) {
-    const {hits, page } = result;
+  }
 
-    const oldHits = page !== 0 ? this.state.results.hits : [];
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+    
+  setSearchTopStories(result) {
+    const {hits, page} = result;
+    const {searchKey, results} = this.state;
+    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
 
     const updatedHits = [
       ...oldHits,
       ...hits
-    ];
+    ]; 
 
     this.setState({ 
-      result: { hits: updatedHits, page }
+      results: {
+        ...results,
+        [searchKey]: {hits: updatedHits, page}
+      }
     });
   }
 
@@ -96,29 +104,26 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, result } = this.state;
-    const page = (result && result.page) || 0;
-
-    if (!result) { return null; }
-
+    const { searchTerm, results, searchKey } = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
+ 
     return (
         <div className="page"> 
           <div className="interactions">
             <Search
               value={searchTerm}
               onChange={this.onSearchChange}
-              onSubmit={this.onSearchSubmit}
+              onSubmit={this.onSearchSubmit} 
+              children="Search"
             />
           </div> 
-          { result
-            ? <Table
-              list={result.hits}
+          <Table
+              list={list}
               onDismiss={this.onDismiss}
-            />
-            : null
-          }
+          />
           <div className="interactions">
-            <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
               More
             </Button>
           </div> 
@@ -127,10 +132,17 @@ class App extends Component {
   } 
 
   onDismiss(id) {
+    const {searchKey, results} = this.state;
+    const {hits, page} = results[searchKey];
+
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.results.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
+
     this.setState({
-      result: {...this.state.results, hits: updatedHits}
+      results: {
+        ...results,
+        [searchKey]: {hits: updatedHits, page}
+      }
     });
   }
     
@@ -141,11 +153,14 @@ class App extends Component {
   onSearchSubmit(event) {
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
-    this.fetchSearchTopStories(searchTerm);
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   }
 
-  fetchSearchTopStories(searchTerm, page = 0) {
+  fetchSearchTopStories(searchTerm, page = 0) { 
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
